94aa8658de982f078b60616e9540adc68d5bb77b
Burkhardt30 <burkhardt30tto@gmail.com>
2021-09-12 18:19:10 +0500

    added FAQ

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,19 @@
-$(function(){
+$(function () {
+
+  $('.faq__item-btn').on('click', function () {
+    if ($(this).prev().children('.faq__item-text').hasClass('faq__item-text--active')) {
+      $(this).prev().children('.faq__item-text').removeClass('faq__item-text--active');
+      $(this).removeClass('faq__item-btn--active');
+      $(this).prev().children('.faq__item-text').slideUp();
+    } else {
+      $('.faq__item-btn').prev().children('.faq__item-text').removeClass('faq__item-text--active');
+      $('.faq__item-btn').removeClass('faq__item-btn--active');
+      $('.faq__item-btn').prev().children('.faq__item-text').slideUp();
+      $(this).prev().children('.faq__item-text').toggleClass('faq__item-text--active');
+      $(this).toggleClass('faq__item-btn--active');
+      $(this).prev().children('.faq__item-text').slideToggle();
+    }
+  });
 
   $('.testimonials__slider').slick({
     arrows: false,
